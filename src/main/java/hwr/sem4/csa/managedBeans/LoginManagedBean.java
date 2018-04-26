6a1bee7b42a7769cde98dedf1337f5fe313d21a5
JohnAEX <grenda.jonathan@gmail.com>
2018-04-26 15:12:36 +0200

    Added C-Validation & Example Loading page
    Updated PF from 6.0 -> 6.2

--- src/main/java/hwr/sem4/csa/managedBeans/LoginManagedBean.java
+++ src/main/java/hwr/sem4/csa/managedBeans/LoginManagedBean.java
@@ -1,8 +1,6 @@
 package hwr.sem4.csa.managedBeans;
 
 import hwr.sem4.csa.database.Databasehandler;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
@@ -15,22 +13,18 @@ public class LoginManagedBean {
     private String password = "";
 
     public String getUsername() {
-        System.out.println("\t>Something happened");
         return username;
     }
 
     public void setUsername(String username) {
-        System.out.println("\t>Something happened2");
         this.username = username;
     }
 
     public String getPassword() {
-        System.out.println("\t>Something happened");
         return password;
     }
 
     public void setPassword(String password) {
-        System.out.println("\t>Something happened2");
         this.password = password;
     }
 
